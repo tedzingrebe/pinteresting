da99a96960f4b76b0280a90ab081c747ee274eee
Ted <ted.zingrebe@gmail.com>
2013-12-19 16:37:35 -0600

    Generating Users with Devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,8 @@ Pinteresting::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  # Required for Heroku - from Devise
+  # Noteto set this to actual host name
+  config.action_mailer.default_url_options = { :host => 'http://tzpinteresting.herokuapp.com' }
 end
